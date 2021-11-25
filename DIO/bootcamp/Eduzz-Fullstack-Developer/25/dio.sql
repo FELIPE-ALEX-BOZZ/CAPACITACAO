ba7d1e795065e76138410e4555a2181967e454fe
FELIPE-ALEX-BOZZ <familiabozz@gmail.com>
2021-11-25 19:04:34 -0300

    MySQL comandos

--- DIO/bootcamp/Eduzz-Fullstack-Developer/25/dio.sql
+++ DIO/bootcamp/Eduzz-Fullstack-Developer/25/dio.sql
@@ -4,6 +4,21 @@ CREATE TABLE pessoas (
     nascimento DATE
 )
 
-INSERT INTO pessoas (nome, nascimento) VALUES('Felipe Alex Bozz','2021-11-25')
-INSERT INTO pessoas (nome, nascimento) VALUES('Pessoa 2','2021-11-26')
+INSERT INTO pessoas (nome, nascimento) VALUES('Felipe Alex Bozz','2021-11-25');
+INSERT INTO pessoas (nome, nascimento) VALUES('Pessoa 2','2021-11-26');
 INSERT INTO pessoas (nome, nascimento) VALUES('Pessoa 3','2021-11-27')
+
+--Aula 02
+
+SELECT * FROM pessoa;
+SELECT nome FROM pessoas;
+
+UPDATE pessoas SET nome='Felipe' WHERE id=1;
+
+DELETE FROM pessoa WHERE id=1; --(Muito cuidado com isso, posso até recuperar a informação mas, o PRIMARY KEY não tem volta)
+
+SELECT * FROM pessoas ORDER BY nome; -- ordem crescente
+SELECT * FROM pessoas ORDER BY nome DESC; -- oredem decrescente
+
+--Como realizar o agrupamento?
+
