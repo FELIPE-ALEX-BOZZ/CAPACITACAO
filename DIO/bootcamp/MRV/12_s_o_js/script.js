5f3427b4e71bd8f11202b4509802300e053b82b1
FELIPE-ALEX-BOZZ <familiabozz@gmail.com>
2021-12-20 22:16:29 -0300

    Exercício operadores

---
+++ DIO/bootcamp/MRV/12_s_o_js/script.js
@@ -0,0 +1,7 @@
+alert('Olá!');
+
+let n1 = Number(prompt('Por favor, digite o primeiro valor:'));
+let n2 = Number(prompt('Por favor, digite o segundo valor:'));
+
+(n1 == n2)?alert('n1 é igual a n2'):alert('n1 é diferente de n2');
+((n1 + n2)>10 && (n1 + n2)<20 )?alert('A soma de n1 + n2 esta entre 10 e 20'):alert('A soma de n1 + n2 não esta entre 10 e 20')
