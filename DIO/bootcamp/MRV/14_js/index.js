53123145a1e4d58030af776b1f211c23838a91fc
FELIPE-ALEX-BOZZ <familiabozz@gmail.com>
2021-12-30 15:49:34 -0300

    Conclusao da aula

--- DIO/bootcamp/MRV/14_js/index.js
+++ DIO/bootcamp/MRV/14_js/index.js
@@ -1,5 +1,17 @@
 function verifPalindromo(string){
-    if(!string) return;
+    if(!string) return "String Inexistente!";
     return string.split("").reverse().join("") === string;
 }
-console.log(verifPalindromo("ana"));
+//console.log(verifPalindromo("ovo"));
+
+//solução 2
+function verifPalindromo2(string){
+    if(!string) return "String Inexistente!";
+    for(let i=0;i<string.length/2;i++){
+        if(string[i]!== string[string.length -1 -i]){
+            return false;
+        }
+        
+    }return true;
+}
+console.log(verifPalindromo2("omo"));
